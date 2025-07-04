75cdffb74daf16562b3f55da49f45fee9c206845
sravs-Junnu19 <lakshmisravya1904@gmail.com>
2025-07-04 09:56:04 +0530

    awsint.py

--- awsint.py
+++ awsint.py
@@ -4,10 +4,10 @@ import uuid
 from datetime import datetime
 
 # Step 1: Create the Flask app instance
-app = Flask(__name__)
+app = Flask(_name_)
 
 # Step 2: Connect to DynamoDB
-dynamodb = boto3.resource('dynamodb', region_name='ap-south-1')  # Replace with your region
+dynamodb = boto3.resource('dynamodb', region_name='us-east-1')  # Replace with your region
 
 # Tables
 photographers_table = dynamodb.Table('photographers')
@@ -57,8 +57,6 @@ def show_photographers():
                            photographers=photographers,
                            availability_data=availability_data)
 
-if __name__ == '__main__':
+if _name_ == 'main':
     app.run(host='0.0.0.0', port=5000, debug=True)
-
-
 
